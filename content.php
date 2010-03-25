d41a4072757beda47f880da2b614430504cc851c
Joseph Gutierrez <gutzofter@yahoo.com>
2010-03-24 23:58:32 -0700

    extracted content so that it could be used in unit testing and main form with php requires

---
+++ content.php
@@ -0,0 +1,10 @@
+<div id="content">
+    <div id="left_container">
+        <div id="input_box" class="outline reset_left left_position">
+
+        </div>
+        <div id="output_box" class="outline reset_left left_position">
+
+        </div>
+    </div>
+</div>
