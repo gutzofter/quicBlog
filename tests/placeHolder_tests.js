bc4ed5d798474326acd8553698d843e324428c84
Joseph Gutierrez <gutzofter@yahoo.com>
2010-03-26 00:37:24 -0700

    adding events to textModel

--- tests/placeHolder_tests.js
+++ tests/placeHolder_tests.js
@@ -1,58 +1,100 @@
-function fakeInput() {
-    var text = '';
-    var whenInput;
+var lambda = {
+    empty: function() {}
+}
 
-    this.set = function(text) {
+function userInput() {
+    this.whenInput = lambda.empty;
+    
+    this.text = '';
+
+    this.setInput = function(text) {
         this.text = text;
         this.whenInput();
-    }
-
-    this.get = function() {
-        return this.text;
     }
 }
 
 function textModel() {
-    this.text = '';
-    this.add = function(character) {
-        this.text = character;
-    }
+    this.whenNewInput = lambda.empty;
     
+    // TODO: add a DataGateway for these memebers
+    this.inputBuffer = '';
+    this.metaInformation = '';
+    //--------------------------------
+
+    this.newInput = function(text) {
+        this.inputBuffer = text;
+        this.whenNewInput();
+    }
+
+    this.placeHolders = function(holders) {
+        this.metaInformation = holders;
+    }
+
+}
+
+function placeHolder() {
+    this.whenParsed = lambda.empty;
+    this.holders = '';
+    this.parseInput = function(inputBuffer) {
+        this.holders = '[hello]: #';
+        this.whenParsed();
+    }
 }
 
 function textController(input, model) {
-
     input.whenInput = function() {
-        model.add(input.get());
+        model.newInput(input.text);
     }
 }
 
+function placeHolderCoordinator(placeHolder, model) {
+    model.whenNewInput = function() {
+        placeHolder.parseInput(model.inputBuffer);
+    }
+
+    placeHolder.whenParsed = function() {
+        model.placeHolders(placeHolder.holders);
+    }
+}
+
+
 $(function(){
 
-    module("textController");
+    module("textModule");
 
     test("initial text should be empty", function(){
-        var input = new fakeInput();
-        var txtModel = new textModel();
-        new textController(input, txtModel);
+        var input = new userInput();
+        var model = new textModel();
+        new textController(input, model);
         
-        equals(txtModel.text, '');
+        ok(validInputBufferOn(model, ''));
     });
 
     test("input text at input should be in model", function(){
-        var input = new fakeInput();
-        var txtModel = new textModel();
-        new textController(input, txtModel);
+        var input = new userInput();
+        var model = new textModel();
+        new textController(input, model);
 
-        input.set('a');
+        input.setInput('a');
 
-        equals(txtModel.text, 'a');
-    });
-
-    module('textModule');
-
-    test('', function () {
+        ok(validInputBufferOn(model, 'a'));
 
     });
+
+     test("add link placeholder", function(){
+        var holder = new placeHolder();
+        var model = new textModel();
+        new placeHolderCoordinator(holder, model);
+
+        model.newInput('[hello]');
+        
+        equal(model.metaInformation, '[hello]: #')
+
+    });
+
+   function validInputBufferOn(model, text) {
+         equals(model.inputBuffer, text);
+         return true;
+    }
 
 });
